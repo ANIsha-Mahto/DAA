59fde395440c3a510764b6914bb071d539b5399e
Anisha Mahto <anisha040mahto@gmail.com>
2025-03-13 11:58:11 +0530

    ACTIVITY_selection

--- ACTIVITY_selection.c
+++ ACTIVITY_selection.c
@@ -1,36 +1,35 @@
-#include<stdio.h>
-int main() {
-    int arr[3][3];
-    for(int j = 0; j < 8; j++) {
-        printf("Enter the %d activity: ", j + 1);
-        scanf("%d", &arr[0][j]);
-        printf("Enter the start time of %d activity: ", j + 1);
-        scanf("%d", &arr[1][j]);
-        printf("Enter the finish time of %d activity: ", j + 1);
-        scanf("%d", &arr[2][j]);
-    }
-        for (int i = 0; i < 2; i++) {
-        for (int j = 0; j < 2- i; j++) {
-            if (arr[2][j] > arr[2][j + 1]) {
-                for (int k = 0; k < 3; k++) {
-                    int temp = arr[k][j];
-                    arr[k][j] = arr[k][j + 1];
-                    arr[k][j + 1] = temp;
-                }
+#include <stdio.h>
+int main()
+{
+    int arr[2][8] = {
+        {1, 3, 0, 5, 8, 5, 12, 2},
+        {2, 4, 6, 7, 9, 9, 14, 12}};
+    int i, j;
+    for (i = 0; i < 8 - 1; i++)
+    {
+        for (j = 0; j < 8 - i - 1; j++)
+        {
+            if (arr[1][j] > arr[1][j + 1])
+            {
+                int temp = arr[0][j];
+                temp = arr[1][j];
+                arr[0][j] = arr[0][j + 1];
+                arr[1][j] = arr[1][j + 1];
+                arr[0][j + 1] = temp;
+                arr[1][j + 1] = temp;
             }
         }
     }
-    printf("The elements of the array are:\n");
-    for(int j=0;j<8;j++){
-printf("A%d\t",j+1);
-    }
-    printf("\n");
-    for(int i = 1; i < 3; i++) {
-        for(int j = 0; j < 8; j++) {
-            printf("%d\t", arr[i][j]);
+    printf("Selected Activity:\n");
+    i = 0;
+    printf("A 1: Start = %d, End = %d\n", arr[0][i], arr[1][i]);
+    for (j = 1; j < 8; j++)
+    {
+  if (arr[0][j] >= arr[1][i])
+        {
+            printf("A %d: Start = %d, End = %d\n", j + 1, arr[0][j], arr[1][j]);
+            i = j;
         }
-        printf("\n");
     }
-
     return 0;
 }
